9e784d3c44686011af8ad79d161ff4039d0a1fc5
Minh Nguyen Cong <clovers1254@gmail.com>
2021-11-22 23:38:07 +0100

    Add GSON

--- src/main/java/org/papz06/App.java
+++ src/main/java/org/papz06/App.java
@@ -1,11 +1,16 @@
 package org.papz06;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    GsonBuilder builder = new GsonBuilder();
+    Gson gson = builder.create();
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
