9fd81f40884315bafe2e0df8efa2c68193d11d5a
DariaKomarynska <dashakomar777@gmail.com>
2021-11-29 17:00:44 +0100

    Add status available for tables except user

--- src/main/db_tables/Cinema_PW.sql
+++ src/main/db_tables/Cinema_PW.sql
@@ -27,7 +27,8 @@ create table rooms
     name varchar2(40) not null,
     rowsNumber number(4),
     seatsInRowNumber number(4),
-    cinema_ID NUMBER NOT NULL CONSTRAINT cin_room_fk REFERENCES cinemas (cinema_ID)
+    cinema_ID NUMBER NOT NULL CONSTRAINT cin_room_fk REFERENCES cinemas (cinema_ID),
+    available number default 1
 );
 ALTER TABLE rooms ADD (
   CONSTRAINT rooms_pk PRIMARY KEY (room_id));
@@ -38,7 +39,8 @@ create table seats
     room_ID NUMBER NOT NULL CONSTRAINT rm_seat_fk REFERENCES rooms (room_ID),
     positionX number(4) NOT NULL,
     positionY number(4) NOT NULL,
-    type VARCHAR2 (40) NOT NULL
+    type VARCHAR2 (40) NOT NULL,
+    available number default 1
 );
 ALTER TABLE seats ADD (
   CONSTRAINT seats_pk PRIMARY KEY (seat_id));
@@ -47,7 +49,8 @@ create table MovieCategories
 (
     MovieCategory_id NUMBER GENERATED ALWAYS AS IDENTITY,
     name varchar2(100) not null,
-    description varchar2(100)
+    description varchar2(100),
+    available number default 1
 );
 ALTER TABLE MovieCategories ADD (
   CONSTRAINT MovieCategories_pk PRIMARY KEY (MovieCategory_id));
@@ -60,7 +63,8 @@ create table movies
     cinema_ID number CONSTRAINT cin_mov_fk REFERENCES cinemas (cinema_ID),
     name varchar2(30) not null,
     description varchar2(100),
-    movieCate_id number constraint mov_cate_fk REFERENCES MovieCategories (MovieCategory_id)
+    movieCate_id number constraint mov_cate_fk REFERENCES MovieCategories (MovieCategory_id),
+    available number default 1
 );
 ALTER TABLE movies ADD (
   CONSTRAINT movies_pk PRIMARY KEY (movie_ID));
@@ -73,7 +77,8 @@ create table Schedules
     room_ID NUMBER NOT NULL CONSTRAINT room_sched_fk REFERENCES rooms (room_ID),
     openSale number not null,
     closeSale number not null,
-    seatLeft number(4) not null
+    seatLeft number(4) not null,
+    available number default 1
 );
 ALTER TABLE Schedules ADD (
   CONSTRAINT Schedules_pk PRIMARY KEY (Schedule_id));
@@ -85,7 +90,8 @@ create table Purchases
     amount float(2) not null,
     paymentMethod varchar2(40),
     currency VARCHAR2(10),
-    schedule_id NUMBER NOT NULL CONSTRAINT sched_purch_fk REFERENCES Schedules (Schedule_id)
+    schedule_id NUMBER NOT NULL CONSTRAINT sched_purch_fk REFERENCES Schedules (Schedule_id),
+    available number default 1
 );
 ALTER TABLE Purchases ADD (
   CONSTRAINT Purchases_pk PRIMARY KEY (Purchase_id));
@@ -95,7 +101,8 @@ create table TicketTypes
     TicketType_id NUMBER GENERATED ALWAYS AS IDENTITY,
     name VARCHAR2 (40) NOT NULL,
     price float(2) not null,
-    cinema_ID NUMBER NOT NULL CONSTRAINT cin_tick_type_fk REFERENCES cinemas (cinema_ID)
+    cinema_ID NUMBER NOT NULL CONSTRAINT cin_tick_type_fk REFERENCES cinemas (cinema_ID),
+    available number default 1
 );
 ALTER TABLE TicketTypes ADD (
   CONSTRAINT TicketTypes_pk PRIMARY KEY (TicketType_id));
@@ -106,7 +113,8 @@ create table Tickets
     Purchase_id NUMBER NOT NULL CONSTRAINT purch_tick_fk REFERENCES Purchases (Purchase_id),
     seat_id NUMBER NOT NULL CONSTRAINT seat_tick_fk REFERENCES seats (seat_id),
     TicketType_id NUMBER NOT NULL CONSTRAINT type_tick_fk REFERENCES TicketTypes (TicketType_id),
-    schedule_id NUMBER NOT NULL CONSTRAINT sched_tick_fk REFERENCES Schedules (Schedule_id)
+    schedule_id NUMBER NOT NULL CONSTRAINT sched_tick_fk REFERENCES Schedules (Schedule_id),
+    available number default 1
 );
 ALTER TABLE Tickets ADD (
   CONSTRAINT Tickets_pk PRIMARY KEY (Ticket_id));
--- src/main/db_tables/Cinema_PW_add_data.sql
+++ src/main/db_tables/Cinema_PW_add_data.sql
@@ -34,104 +34,106 @@ insert into cinemas values (default, 0002, 'Atlantic Kino', default);
 insert into cinemas values (default, 0003, 'Multikino', default);
 commit;
 -------------------------
-insert into rooms values (default, 'Room 1', 12, 20, 1);
-insert into rooms values (default, 'Room 2', 24, 40, 1);
-insert into rooms values (default, 'Room 3', 20, 30, 1);
-insert into rooms values (default, 'Room 1', 12, 20, 2);
-insert into rooms values (default, 'Room 2', 24, 40, 2);
-insert into rooms values (default, 'Room 1', 20, 30, 3);
-insert into rooms values (default, 'Room 2', 24, 30, 3);
+
+insert into rooms values (default, 'Room 1', 12, 20, 1, default);
+insert into rooms values (default, 'Room 2', 24, 40, 1, default);
+insert into rooms values (default, 'Room 3', 20, 30, 1, default);
+insert into rooms values (default, 'Room 1', 12, 20, 2, default);
+insert into rooms values (default, 'Room 2', 24, 40, 2, default);
+insert into rooms values (default, 'Room 1', 20, 30, 3, default);
+insert into rooms values (default, 'Room 2', 24, 30, 3, default);
+commit;
+-------------------------
+
+insert into seats values (default, 1, 2, 2, 'comfort', default);
+insert into seats values (default, 1, 2, 3, 'comfort', default);
+insert into seats values (default, 1, 2, 4, 'comfort', default);
+insert into seats values (default, 1, 2, 5, 'luxury', default);
+
+
+insert into seats values (default, 2, 1, 2, 'comfort', default);
+insert into seats values (default, 2, 1, 3, 'comfort', default);
+insert into seats values (default, 2, 2, 1, 'comfort', default);
+insert into seats values (default, 2, 3, 6, 'comfort', default);
+insert into seats values (default, 2, 3, 7, 'comfort', default);
+insert into seats values (default, 2, 3, 8, 'comfort', default);
+insert into seats values (default, 2, 3, 9, 'luxury', default);
+insert into seats values (default, 2, 4, 1, 'luxury', default);
+
+insert into seats values (default, 3, 2, 5, 'comfort', default);
+insert into seats values (default, 3, 2, 6, 'comfort', default);
+insert into seats values (default, 3, 2, 7, 'luxury', default);
+insert into seats values (default, 3, 2, 8, 'luxury', default);
+insert into seats values (default, 3, 2, 9, 'luxury', default);
+insert into seats values (default, 3, 2, 10, 'luxury', default);
+
+insert into seats values (default, 4, 1, 2, 'comfort', default);
+insert into seats values (default, 4, 2, 3, 'comfort', default);
+
 commit;
 -------------------------
 --work
-insert into seats values (default, 1, 2, 2, 'comfort');
-insert into seats values (default, 1, 2, 3, 'comfort');
-insert into seats values (default, 1, 2, 4, 'comfort');
-insert into seats values (default, 1, 2, 5, 'luxury');
-
-insert into seats values (default, 2, 1, 2, 'comfort');
-insert into seats values (default, 2, 1, 3, 'comfort');
-insert into seats values (default, 2, 2, 1, 'comfort');
-insert into seats values (default, 2, 3, 6, 'comfort');
-insert into seats values (default, 2, 3, 7, 'comfort');
-insert into seats values (default, 2, 3, 8, 'comfort');
-insert into seats values (default, 2, 3, 9, 'luxury');
-insert into seats values (default, 2, 4, 1, 'luxury');
-
-insert into seats values (default, 3, 2, 5, 'comfort');
-insert into seats values (default, 3, 2, 6, 'comfort');
-insert into seats values (default, 3, 2, 7, 'luxury');
-insert into seats values (default, 3, 2, 8, 'luxury');
-insert into seats values (default, 3, 2, 9, 'luxury');
-insert into seats values (default, 3, 2, 10, 'luxury');
-
-insert into seats values (default, 4, 1, 2, 'comfort');
-insert into seats values (default, 4, 2, 3, 'comfort');
-
-commit;
--------------------------
---work
-insert into MovieCategories values (default, 'Horror', 'funny');
-insert into MovieCategories values (default, 'Romance', 'funny');
-insert into MovieCategories values (default, 'Thriller', 'funny');
-insert into MovieCategories values (default, 'drama', 'funny');
-insert into MovieCategories values (default, 'film', 'funny');
-insert into MovieCategories values (default, 'Action', 'funny');
-insert into MovieCategories values (default, 'romantic', 'funny');
+insert into MovieCategories values (default, 'Horror', 'funny', default);
+insert into MovieCategories values (default, 'Romance', 'funny', default);
+insert into MovieCategories values (default, 'Thriller', 'funny', default);
+insert into MovieCategories values (default, 'drama', 'funny', default);
+insert into MovieCategories values (default, 'film', 'funny', default);
+insert into MovieCategories values (default, 'Action', 'funny', default);
+insert into MovieCategories values (default, 'romantic', 'funny', default);
 commit;
 -------------------------
 
-insert into movies values (default, 90, '14+', 1, '50 shades of grey', 'romantic', 1);
-insert into movies values (default, 120, '18+', 1, '51 shades of grey', 'drama', 3);
-insert into movies values (default, 140, '18+', 1, '52 shades of grey', 'film', 2);
-insert into movies values (default, 120, '14+', 2, '53 shades of grey', 'Horror', 4);
-insert into movies values (default, 135, '16+', 1, 'Spider-Man 1', 'Action', 6);
-insert into movies values (default, 140, '18+', 2, 'Spider-Man 2', 'Romance', 5);
-insert into movies values (default, 90, '18+', 1, 'Spider-Man 3', 'Thriller', 7);
-insert into movies values (default, 90, '16+', 2, 'The Matrix 1', 'romantic', 5);
-insert into movies values (default, 140, '6+', 1, 'The Matrix 2', 'Horror', 7);
-insert into movies values (default, 120, '14+', 3, 'The Matrix 3', 'Romance', 3);
-insert into movies values (default, 135, '18+', 3, 'The Matrix 4', 'Thriller', 3);
-insert into movies values (default, 140, '14+', 3, 'The Matrix 5', 'drama', 1);
-insert into movies values (default, 135, '6+', 2, 'Evil 1', 'film', 2);
-insert into movies values (default, 140, '18+', 1, 'Evil 2', 'Action', 5);
-insert into movies values (default, 90, '6+', 1, 'Evil 3', 'romantic', 6);
+insert into movies values (default, 90, '14+', 1, '50 shades of grey', 'romantic', 1, default);
+insert into movies values (default, 120, '18+', 1, '51 shades of grey', 'drama', 3, default);
+insert into movies values (default, 140, '18+', 1, '52 shades of grey', 'film', 2, default);
+insert into movies values (default, 120, '14+', 2, '53 shades of grey', 'Horror', 4, default);
+insert into movies values (default, 135, '16+', 1, 'Spider-Man 1', 'Action', 6, default);
+insert into movies values (default, 140, '18+', 2, 'Spider-Man 2', 'Romance', 5, default);
+insert into movies values (default, 90, '18+', 1, 'Spider-Man 3', 'Thriller', 7, default);
+insert into movies values (default, 90, '16+', 2, 'The Matrix 1', 'romantic', 5, default);
+insert into movies values (default, 140, '6+', 1, 'The Matrix 2', 'Horror', 7, default);
+insert into movies values (default, 120, '14+', 3, 'The Matrix 3', 'Romance', 3, default);
+insert into movies values (default, 135, '18+', 3, 'The Matrix 4', 'Thriller', 3, default);
+insert into movies values (default, 140, '14+', 3, 'The Matrix 5', 'drama', 1, default);
+insert into movies values (default, 135, '6+', 2, 'Evil 1', 'film', 2, default);
+insert into movies values (default, 140, '18+', 1, 'Evil 2', 'Action', 5, default);
+insert into movies values (default, 90, '6+', 1, 'Evil 3', 'romantic', 6, default);
 commit;
 -------------------------
 
-insert into Schedules values (default, 1638057506,  1, 1, 1628935106, 1629107906, 2);
-insert into Schedules values (default, 1637971106,  1, 2, 1621159106, 1621589106, 2);
-insert into Schedules values (default, 1635292706,  1, 2, 1621763906, 1621764506, 2);
-insert into Schedules values (default, 1635260306,  2, 3, 1619168400, 1619258400, 2);
-insert into Schedules values (default, 1635245906,  2, 3, 1618390800, 1618342800, 2);
-insert into Schedules values (default, 1635159506,  3, 4, 1618230800, 1618375800, 2);
+insert into Schedules values (default, 1638057506,  1, 1, 1628935106, 1629107906, 2, default);
+insert into Schedules values (default, 1637971106,  1, 2, 1621159106, 1621589106, 2, default);
+insert into Schedules values (default, 1635292706,  1, 2, 1621763906, 1621764506, 2, default);
+insert into Schedules values (default, 1635260306,  2, 3, 1619168400, 1619258400, 2, default);
+insert into Schedules values (default, 1635245906,  2, 3, 1618390800, 1618342800, 2, default);
+insert into Schedules values (default, 1635159506,  3, 4, 1618230800, 1618375800, 2, default);
 commit;
 -------------------------
 
-insert into Purchases values (default, 1635159506, 80.00, 'Credit card', 'usd', 1);
-insert into Purchases values (default, 1632567506, 40.00, 'Cash', 'uah', 1);
-insert into Purchases values (default, 1631617106, 80.00, 'Credit card', 'usd', 2);
-insert into Purchases values (default, 1631620706, 40.00,'Credit card', 'pln', 2);
-insert into Purchases values (default, 1631617106, 40.00, 'Cash', 'uah', 2);
-insert into Purchases values (default, 1631613506, 80.00, 'Credit card', 'usd', 3);
+insert into Purchases values (default, 1635159506, 80.00, 'Credit card', 'usd', 1, default);
+insert into Purchases values (default, 1632567506, 40.00, 'Cash', 'uah', 1, default);
+insert into Purchases values (default, 1631617106, 80.00, 'Credit card', 'usd', 2, default);
+insert into Purchases values (default, 1631620706, 40.00,'Credit card', 'pln', 2, default);
+insert into Purchases values (default, 1631617106, 40.00, 'Cash', 'uah', 2, default);
+insert into Purchases values (default, 1631613506, 80.00, 'Credit card', 'usd', 3, default);
 commit;
 -------------------------
 
-insert into TicketTypes values (default, 'adult', 80.00, 1);
-insert into TicketTypes values (default, 'child', 40.00, 1);
-insert into TicketTypes values (default, 'adult', 60.00, 2);
-insert into TicketTypes values (default, 'child', 30.00, 2);
-insert into TicketTypes values (default, 'adult', 70.00, 3);
-insert into TicketTypes values (default, 'child', 20.00, 3);
+insert into TicketTypes values (default, 'adult', 80.00, 1, default);
+insert into TicketTypes values (default, 'child', 40.00, 1, default);
+insert into TicketTypes values (default, 'adult', 60.00, 2, default);
+insert into TicketTypes values (default, 'child', 30.00, 2, default);
+insert into TicketTypes values (default, 'adult', 70.00, 3, default);
+insert into TicketTypes values (default, 'child', 20.00, 3, default);
 commit;
 -------------------------
 
-insert into Tickets values (default, 1, 2, 1, 1);
-insert into Tickets values (default, 1, 3, 1, 2);
-insert into Tickets values (default, 2, 6, 4, 2);
-insert into Tickets values (default, 2, 7, 3, 3);
-insert into Tickets values (default, 2, 8, 4, 3);
-insert into Tickets values (default, 3, 14, 6, 4);
+insert into Tickets values (default, 1, 2, 1, 1, default);
+insert into Tickets values (default, 1, 3, 1, 2, default);
+insert into Tickets values (default, 2, 6, 4, 2, default);
+insert into Tickets values (default, 2, 7, 3, 3, default);
+insert into Tickets values (default, 2, 8, 4, 3, default);
+insert into Tickets values (default, 3, 14, 6, 4, default);
 commit;
 -------------------------
 
