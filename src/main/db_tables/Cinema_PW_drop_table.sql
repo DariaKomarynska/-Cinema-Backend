01a4a130f76def33308bac6d5cbd10d1433cf321
Truong Giang Do <01165758@pw.edu.pl>
2022-01-11 21:07:00 +0100

    Add test Schedule

--- src/main/db_tables/Cinema_PW_drop_table.sql
+++ src/main/db_tables/Cinema_PW_drop_table.sql
@@ -9,11 +9,11 @@ drop table Tickets;
 drop table Purchases CASCADE CONSTRAINTS;
 drop table TicketTypes;
 
-update seats set available = 0 where seat_id = 3;
-
-insert into purchases values (default, 4564654123, 500, default, default, 3, default);
-
-select * from purchases where available = 1 
-order by purchase_id desc fetch next 1 rows only;
-
-insert into tickets values (default, 21, 3, 5, 3, default);
+--update seats set available = 0 where seat_id = 3;
+--
+--insert into purchases values (default, 4564654123, 500, default, default, 3, default);
+--
+--select * from purchases where available = 1 
+--order by purchase_id desc fetch next 1 rows only;
+--
+--insert into tickets values (default, 21, 3, 5, 3, default);
