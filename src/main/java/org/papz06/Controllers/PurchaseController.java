e10d4bd087625226f16859dcb410b8b9ac876ce6
DariaKomarynska <dashakomar777@gmail.com>
2022-01-12 23:44:52 +0100

    Working purchase acceptment api

--- src/main/java/org/papz06/Controllers/PurchaseController.java
+++ src/main/java/org/papz06/Controllers/PurchaseController.java
@@ -3,10 +3,7 @@ package org.papz06.Controllers;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.papz06.Function;
-import org.papz06.Models.Purchase;
-import org.papz06.Models.Room;
-import org.papz06.Models.Ticket;
-import org.papz06.Models.TicketType;
+import org.papz06.Models.*;
 
 import java.sql.ResultSet;
 import java.time.Instant;
@@ -21,7 +18,6 @@ public class PurchaseController {
         try {
             rs = fc.executeQuery("select * from purchases");
             while (rs.next()) {
-                System.out.println("hmmm where");
                 purchasesList.add(
                         new Purchase(rs.getInt(1),
                                 rs.getInt(2),
@@ -30,13 +26,30 @@ public class PurchaseController {
                                 rs.getString(5),
                                 rs.getInt(6))
                 );
-                System.out.println("okay or no");
             }
             fc.closeQuery();
         } catch (Exception e) {
             System.out.println(e);
         }
         return purchasesList;
+    }
+
+    public static JSONArray getPurchasesId() {
+        JSONArray resultData = new JSONArray();
+        Function fc = new Function();
+        ResultSet rs;
+        try {
+            rs = fc.executeQuery("select purchase_id from purchases where available = 1");
+            while (rs.next()) {
+                JSONObject purchaseData = new JSONObject();
+                purchaseData.put("id", rs.getInt(1));
+                resultData.put(purchaseData);
+            }
+            fc.closeQuery();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        return resultData;
     }
 
     public static JSONObject getPurchaseById(int id) {
@@ -109,43 +122,43 @@ public class PurchaseController {
         Function fc = new Function();
         JSONObject purchaseData = new JSONObject();
         try {
-            System.out.println("111");
             String sqlUpdate = String.format(
                     "update purchases set paymentMethod = '%s', currency = '%s' where purchase_id = %d and available = 1",
                     paymentMethod, currency, purchaseId);
             fc.executeQuery(sqlUpdate);
-            System.out.println("222");
             int amount = getPurchaseById(purchaseId).getInt("amount");
-            System.out.println("33");
             int scheduleId = getPurchaseById(purchaseId).getInt("schedule_id");
-            System.out.println("44");
             int movie_id = ScheduleController.getPurchaseInfo(scheduleId).getInt("movie_id");
-            System.out.println("55");
             int room_id = ScheduleController.getPurchaseInfo(scheduleId).getInt("room_id");
-            System.out.println("66");
 
             String movieName = MovieController.getMovieById(movie_id).getName();
             String roomName = RoomController.getRoomById(room_id).getName();
             JSONArray tickets = TicketController.getBoughtTicketsInfo(purchaseId);
-            System.out.println("tickets  " + tickets);
-            System.out.println(tickets);
             purchaseData.put("id", purchaseId);
             purchaseData.put("amount", amount);
             purchaseData.put("movieName", movieName);
             purchaseData.put("roomName", roomName);
             purchaseData.put("tickets", tickets);
-            System.out.println("okay bye");
+            int seatsBusy = tickets.length();
+            String updateSeatLeft = String.format("update schedules set seatleft = seatleft - %d where schedule_id = %d", seatsBusy, scheduleId);
+            fc.executeQuery(updateSeatLeft);
             fc.closeQuery();
         } catch (Exception e) {
             System.out.println("Exception: " + e);
+            return null;
         }
         return purchaseData;
     }
 
     public static boolean checkExist(int id){
-        System.out.println(getPurchasesList());
-        for (Purchase purchase : getPurchasesList()) {
-            if (purchase.getId() == id) {
+        System.out.println(getPurchasesId());
+        JSONArray purchasesId = getPurchasesId();
+        JSONObject purchId = new JSONObject();
+        int existId;
+        for (int i = 0; i < purchasesId.length(); ++i){
+            purchId = purchasesId.getJSONObject(i);
+            existId = purchId.getInt("id");
+            if (existId == id){
                 return true;
             }
         }
